d7c448d845add0db936aeda74d0d116ccce5888e
Akmal <akmal_179a@mail.ru>
2022-08-31 23:46:51 +0500

    Values added

---
+++ Example_006/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 10;
+int b = 18;
+int c = 21;
+int d = 17;
+int e = 22;
+
+int max = a;
+
+if (a < b) max = b;
+if (b < c) max = c;
+if (c < d) max = d;
+if (d < e) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
