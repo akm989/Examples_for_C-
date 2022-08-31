d8b78fc9e57fca81a83d9a2cb4112dba82781a36
Akmal <akmal_179a@mail.ru>
2022-08-31 23:29:11 +0500

    Conditions added

---
+++ Example_005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Type username: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() =="akmal")
+{
+    Console.WriteLine("Yaaaay its you AY!");
+}
+
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
